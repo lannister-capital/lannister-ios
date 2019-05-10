6a6fe05a2944cb6312d165317a82e7e9a07cfb8b
André Sousa <andre.sousa07@gmail.com>
2019-05-11 00:14:00 +0100

    Blockstack auth.

--- Lannister/Controllers/AuthController.swift
+++ Lannister/Controllers/AuthController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Blockstack
 
 class AuthController: UIViewController {
     
@@ -27,8 +28,24 @@ class AuthController: UIViewController {
         DispatchQueue.main.asyncAfter(deadline: .now(), execute: {
             
             UserDefaults.standard.setValue("auth", forKey: "Auth")
-            AppDelegate.shared.rootViewController.switchToMainScreen()
-            AppDelegate.shared.updateCurrencies()
+            
+            Blockstack.shared.signIn(redirectURI: "https://lannister.capital/redirect-mobile.html",
+                                     appDomain: URL(string: "https://lannister.capital")!,
+                                     manifestURI: nil,
+                                     scopes: ["store_write", "publish_data"]) { authResult in
+                                        switch authResult {
+                                        case .success(let userData):
+                                            print("Sign in SUCCESS", userData.profile?.name as Any)
+                                            DispatchQueue.main.asyncAfter(deadline: .now(), execute: {
+                                                AppDelegate.shared.rootViewController.switchToMainScreen()
+                                                AppDelegate.shared.updateCurrencies()
+                                            })
+                                        case .cancelled:
+                                            print("Sign in CANCELLED")
+                                        case .failed(let error):
+                                            print("Sign in FAILED, error: ", error ?? "n/a")
+                                        }
+            }
         })
     }
     
