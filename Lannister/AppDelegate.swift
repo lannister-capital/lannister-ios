1ba2b5b13e9c0ed4efc2fa70470c12a3f4f3b091
André Sousa <andre.sousa07@gmail.com>
2019-05-27 13:50:58 +0100

    Blur background when app resigns active.

--- Lannister/AppDelegate.swift
+++ Lannister/AppDelegate.swift
@@ -32,6 +32,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        if let bioAccess = UserDefaults.standard.object(forKey: "bioAccess") as? Bool {
+            if bioAccess == true {
+                blurBackground()
+            }
+        }
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
